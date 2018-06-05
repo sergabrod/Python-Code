8edadd8bec03362485a79c6bfbc969693d343b66
Sergio Gabriel <sgrodriguez@gmail.com>
2018-06-05 09:07:48 -0300

    Agrego mas funciones de listas

--- listas.py
+++ listas.py
@@ -7,7 +7,22 @@ print(servidores)
 
 #Posición de i hasta j (no inclusive)
 print(servidores[1:3])
+
+#Posición de 2 hasta el final
+print(servidores[2:])
+
 print(servidores[-2])
 
 print('server2' in servidores)
+# Agrega un elemento al final de la lista
+servidores.append('servidor-bd')
+
+# Agrega un elemento en una posición
+servidores.insert(3, 'server4')
+print(servidores)
+
+# Agrega varios elementos a la lista
+servidores.extend(['server5', 'server6'])
+print(servidores)
+
 
