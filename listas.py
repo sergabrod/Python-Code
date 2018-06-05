5e9e4ca63f39d1fdebc346883a642bf5d25d5345
Sergio Gabriel <sgrodriguez@gmail.com>
2018-06-05 08:59:04 -0300

    modifico lista

--- listas.py
+++ listas.py
@@ -1,13 +1,13 @@
 # Listas en Python
 
-servidores = ['vargas1', 'vargas2', 'vargas3', 'iol-web']
+servidores = ['server1', 'server2', 'server3', 'server-web']
 print (servidores[0])
-servidores[0] = 'vargas1-db'
+servidores[0] = 'server-db'
 print(servidores)
 
 #Posición de i hasta j (no inclusive)
 print(servidores[1:3])
 print(servidores[-2])
 
-print('vargas2' in servidores)
+print('server2' in servidores)
 
