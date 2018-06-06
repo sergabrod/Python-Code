ea0c2a01fa46f48b724dbd2e7c5c13c5023c1d17
Sergio Gabriel <sgrodriguez@gmail.com>
2018-06-06 06:40:52 -0300

    Utilizando tuplas, métodos

--- tuplas.py
+++ tuplas.py
@@ -4,11 +4,33 @@
 # Son más rápidas, menos espacio, formatean string.
 # Se pueden utilizar como claves de un diccionario (las listas no)
 
-usuarios = ('usuario1', 'usuario2', 'usuario3', 'usuario4', 7)
+usuarios = ('usuario1', 'usuario2', 'usuario3', 'usuario4', 7, 'usuario1')
 print(usuarios)
 print(usuarios[2])
 
 # Para convertir una tupla en una lista
-
 lista_usuarios = list(usuarios)
 print(lista_usuarios)
+
+# Para convertir una lista en una tupla
+usuarios = tuple(lista_usuarios)
+print(usuarios)
+
+# Para verificar si un elemento existe en la tupla
+print('usuario3' in usuarios)
+
+# Averiguar cuántos elementos se encuentran en la tupla
+print(usuarios.count('usuario1'))
+
+# Averiguar la longitud de la tupla
+print(len(usuarios))
+
+# Una tupla unitaria lleva una coma al final del único elemento
+usuario = ("Sergio",)
+print(usuario)
+
+# Asignar una tupla a variables (desempaquetado de tuplas)
+registro = ('Sergio', 5, 6, 2018)
+nombre, dia, mes, anio = registro
+print("Su nombre es " + nombre + ", hoy es " + str(dia) + '/' + str(mes) + '/' + str(anio))
+
