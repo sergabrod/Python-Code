512edd7a0ae76cf20978d0321f69a0be30a03371
Sergio Gabriel <sgrodriguez@gmail.com>
2018-07-10 08:45:41 -0300

    Trabajando con archivos externos - lectura y escritura

--- archivos/file_manager.py
+++ archivos/file_manager.py
@@ -2,10 +2,19 @@
 # Trabajando con archivos y el módulo io
 from io import open
 
-"""Abrimos el archivo en modo escritura"""
-archivo_texto = open("archivo.txt", "w")
-
-frase = "Hoy es un muy buen día para estudiar Python.\nBuen Martes."
-archivo_texto.write(frase)
+"""Abrimos el archivo en modo lectura"""
+archivo_texto = open("archivo.txt", "r")
+# texto = archivo_texto.read()
+# Devuelve una lista donde cada renglón es un elemento
+lineas_texto = archivo_texto.readlines()
 archivo_texto.close()
+print(lineas_texto[0])
+
+"""Abrimos el archivo en modo de agregar"""
+archivo_texto = open("archivo.txt", "a")
+archivo_texto.write("\nAquí agregamos una línea al texto")
+archivo_texto.close()
+
+
+
 
