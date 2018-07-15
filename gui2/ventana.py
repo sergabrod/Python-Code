5619540eebce126617f8a8ffe00ec9e38f5543ec
Sergio Gabriel <sgrodriguez@gmail.com>
2018-07-15 17:16:33 -0300

    Iniciando Tkinter - usando Frames

--- gui2/ventana.py
+++ gui2/ventana.py
@@ -11,6 +11,15 @@ root.resizable(True, True)
 # Cambia el color de fondo
 root.config(bg="blue")
 # root.iconbitmap("python.png")
+# Creo un frame y lo agrego a la raiz (root), antes debemos empaquetarlo
+miFrame = Frame()
+# anchor posiciona segùn los puntos cardinales, complento de side
+miFrame.pack(side="bottom", anchor="w")
+miFrame.config(bg="white")
+miFrame.config(width="650", height="300")
+miFrame.config(bd=5) # Borde
+miFrame.config(cursor="hand2")
+miFrame.config(relief="groove")
 # Bucle infinito para permanecer a la escucha de eventos
 root.mainloop()
 
