881b3e52d97eb95339c27a141b94d362891b09a4
Sergio Gabriel <sgrodriguez@gmail.com>
2018-07-18 11:04:43 -0300

    Ventanas emergentes con Tkinter

--- gui2/menues.py
+++ gui2/menues.py
@@ -2,9 +2,40 @@
 # utilizando widget menú
 
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 root.title("Uso de Menúes")
+
+
+def acerca_de():
+    messagebox.showinfo("Sistema de Ventas", "SV - Python 2018")
+
+
+def documentacion():
+    messagebox.showwarning("Aviso!", "Documentación aún no disponible")
+
+
+def salir():
+    valor = messagebox.askquestion("Salir", "¿Desea salir de la aplicación?")
+    if valor == "yes":
+        root.destroy()
+
+
+def analizar_codigo():
+    valor = messagebox.askokcancel("Analize", "Esto puede tardar mucho ¿Desea realizarlo de todos modos?")
+    if valor:
+        print("Analizando codigo....")
+    else:
+        print("Analisis cancelado.")
+
+
+def ssh_session():
+    valor = messagebox.askretrycancel("SSH", "No se puede establecer la conexión con el servidor")
+    if valor:
+        print("Intentando conectar son SSH....")
+    else:
+        print("Conexión SSH cancelada.")
 
 barra_menu = Menu(root)
 root.config(menu=barra_menu, width=300, height=300)
@@ -16,7 +47,7 @@ archivo_menu.add_command(label="Guardar")
 archivo_menu.add_command(label="Guardar como...")
 archivo_menu.add_separator()
 archivo_menu.add_command(label="Cerrar")
-archivo_menu.add_command(label="Salir")
+archivo_menu.add_command(label="Salir", command=salir)
 
 edicion_menu = Menu(barra_menu, tearoff=0)
 edicion_menu.add_command(label="Deshacer")
@@ -33,13 +64,13 @@ herramientas_menu = Menu(barra_menu, tearoff=0)
 herramientas_menu.add_command(label="Consola")
 herramientas_menu.add_command(label="Git")
 herramientas_menu.add_separator()
-herramientas_menu.add_command(label="SSH Session")
+herramientas_menu.add_command(label="SSH Session", command=ssh_session)
 herramientas_menu.add_command(label="Deploy")
-herramientas_menu.add_command(label="Analizar Código")
+herramientas_menu.add_command(label="Analizar Código", command=analizar_codigo)
 
 ayuda_menu = Menu(barra_menu, tearoff=0)
-ayuda_menu.add_command(label="Documentación")
-ayuda_menu.add_command(label="Acerca de...")
+ayuda_menu.add_command(label="Documentación", command=documentacion)
+ayuda_menu.add_command(label="Acerca de...", command=acerca_de)
 
 # ------------------Agregamos a la barra--------------------
 barra_menu.add_cascade(label="Archivo", menu=archivo_menu)
